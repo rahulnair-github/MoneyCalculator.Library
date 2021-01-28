6dd91380b8dde961dd8e2473b62f8bd70347bf2d
Rahul Nair <rahulnair22@outlook.com>
2021-01-28 00:41:19 +0000

    Added MoneyCalculator class and associated failing tests

---
+++ MoneyCalculator.Test/MoneyCalculatorTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MoneyCalculator.Test
+{
+    public class MoneyCalculatorTest
+    {
+        [Fact]
+        public void MaxMoneyTest()
+        {
+            IMoneyCalculator moneyCalculator = new MoneyCalculator();
+            var monies = GetMoneyList();
+            var expected = monies.Max(x => x.Amount);
+            var result = moneyCalculator.Max(monies);
+            Assert.Equal(expected, result.Amount);
+        }
+
+        [Fact]
+        public void MaxforMoniesinDifferentCurrenciesTest()
+        {
+            IMoneyCalculator moneyCalculator = new MoneyCalculator();
+            var monies = GetMoneyListWithMultipleCurrencies();
+            Assert.Throws<ArgumentException>(() => moneyCalculator.Max(monies));
+        }
+        [Fact]
+        public void MaxTestforNullMonies()
+        {
+            IMoneyCalculator moneyCalculator = new MoneyCalculator();
+            Assert.Throws<ArgumentNullException>(() => moneyCalculator.Max(null));
+        }
+
+        [Fact]
+        public void SumPerCurrencyTest()
+        {
+            IMoneyCalculator moneyCalculator = new MoneyCalculator();
+            IEnumerable<IMoney> monies = GetMoneyListWithMultipleCurrencies();
+            var actual = moneyCalculator.SumPerCurrency(monies);
+            var expected = new List<IMoney> {
+                new Money(currencyCode:"GBP",amount:300.00M),
+                new Money(currencyCode:"USD",amount:350.00M)
+            };
+
+            var equality = CheckMoneyListEquality(actual, expected);
+
+            Assert.True(equality);
+
+        }
+
+        [Fact]
+        public void SumPerCurrencyNullTest()
+        {
+            IMoneyCalculator moneyCalculator = new MoneyCalculator();
+            Assert.Throws<ArgumentNullException>(()=> moneyCalculator.SumPerCurrency(null));
+
+        }
+
+
+
+
+        #region Private Methods
+
+        private bool CheckMoneyListEquality(IEnumerable<IMoney> actual, IEnumerable<IMoney> expected)
+        {
+            if (actual.Count() != expected.Count()) return false;
+
+            foreach (var item in actual)
+            {
+                var expectedItem = expected.FirstOrDefault(i => i.Currency == item.Currency);
+                if (expectedItem == null) return false;
+
+                if (expectedItem.Amount != item.Amount) return false;
+
+            }
+            return true;
+        }
+        private IEnumerable<IMoney> GetMoneyListWithMultipleCurrencies()
+        {
+            return new List<IMoney> {
+                new Money(100.00M, "GBP"),
+                new Money(200.00M, "GBP"),
+
+                new Money(100.00M, "USD"),
+                new Money(250.00M, "USD"),
+
+            };
+        }
+
+        private IEnumerable<IMoney> GetMoneyList()
+        {
+            return new List<IMoney> {
+                new Money(100.00M, "GBP"),
+                new Money(200.00M, "GBP"),
+                new Money(300.00M, "GBP"),
+            };
+        }
+        #endregion
+    }
+}
+
---
+++ MoneyCalculator/MoneyCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyCalculator
+{
+    public class MoneyCalculator : IMoneyCalculator
+    {
+        public IMoney Max(IEnumerable<IMoney> monies)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public IEnumerable<IMoney> SumPerCurrency(IEnumerable<IMoney> monies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
